d1894145a92986f7edb39ab6571395cd83f0f656
dr0138 <dr0138>
2024-07-26 10:18:20 +0000

    commit & push
    Import very first file : list of columns

---
+++ list_of_columns.py
@@ -0,0 +1,29 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC #### List of columns
+
+# COMMAND ----------
+
+list_of_columns={
+  'columns_list':
+      ['call_id',
+       'channel_type',
+       'network_transport_group',
+       'customer_products_WIRLS',
+       'customer_products_POTS',
+       'customer_products_VOIP',
+       'customer_products_CONNCAR',
+       'customer_products_DSL',
+       'customer_products_VOIP_OOF',
+       'customer_products_WHP',
+       'customer_products_WBB'
+       ]
+}
+
+# COMMAND ----------
+
+# type(list_of_columns)
+
+# COMMAND ----------
+
+
